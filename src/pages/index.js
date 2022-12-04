233edcb1f1d338d5342abc7f452bc1cac3a6a170
Matt <matt@spectrebot.net>
2022-12-04 15:15:45 +0000

    reloacte files

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+        <Head>
+            <title>OrcaSquad</title>
+            <meta name="description" content=" " />
+            
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        </div>
+    )
+}
