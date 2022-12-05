f3d8f526dcb6e8ff98e36dadc635c1c353259f46
Louise <louise@louisefindlay.com>
2022-12-05 20:07:47 +0000

    Created Header Component (#6)
    * Created Flexbox header to mimic design
    
    * Removed nav buttons
    
    Co-authored-by: Matt Davison <matt@mjdws.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
-
+import styles from "../styles/Header.module.css";
 
 export default function Header() {
     return (
         <div>
-            <header>
-
+            <header className={styles.header}>
+                <div className={styles.wrapper}>
+                    <a className={styles.title} href="#">
+                        OrcaSquad
+                    </a>
+                    <nav className={styles.nav}>
+                        <ul>
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">About</a>
+                            </li>
+                            <li>
+                                <a href="#">Discord</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </header>
         </div>
-    )
+    );
 }
